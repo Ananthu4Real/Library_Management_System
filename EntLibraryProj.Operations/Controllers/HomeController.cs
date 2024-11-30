5c3811ff47928926d6740b0e7c489b31368d3e03
Ananthakrishnan <thurutha@sheridancollege.ca>
2024-11-30 16:29:39 -0500

    Added CSS to Nav Buttons (FAQ and Explore Events)

--- EntLibraryProj.Operations/Controllers/HomeController.cs
+++ EntLibraryProj.Operations/Controllers/HomeController.cs
@@ -27,20 +27,4 @@ namespace EntLibraryProj.Operations.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult FAQ()
-    {
-        var faqList = new List<FAQModel>
-        {
-            new FAQModel { Id = 1, Question = "What are the library hours?", Answer = "We are open from 9 AM to 8 PM on weekdays and 10 AM to 6 PM on weekends." },
-            new FAQModel { Id = 2, Question = "Can I renew a book online?", Answer = "Yes, you can renew books through your account on our website." },
-            new FAQModel { Id = 3, Question = "How many items can I borrow at once?", Answer = "You can borrow up to 10 items at a time." }
-        };
-
-        return View(faqList);
-    }
-
-    }
-}
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIden
