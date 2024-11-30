0199402d8116857137021beec7ce7c4eef3f8d38
Ananthakrishnan <thurutha@sheridancollege.ca>
2024-11-30 16:23:01 -0500

    Added the FAQ method in the Home Controller

--- EntLibraryProj.Operations/Controllers/HomeController.cs
+++ EntLibraryProj.Operations/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using EntLibraryProj.Models;
 using EntLibraryProj.Operations.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -28,5 +29,18 @@ namespace EntLibraryProj.Operations.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FAQ()
+    {
+        var faqList = new List<FAQModel>
+        {
+            new FAQModel { Id = 1, Question = "What are the library hours?", Answer = "We are open from 9 AM to 8 PM on weekdays and 10 AM to 6 PM on weekends." },
+            new FAQModel { Id = 2, Question = "Can I renew a book online?", Answer = "Yes, you can renew books through your account on our website." },
+            new FAQModel { Id = 3, Question = "How many items can I borrow at once?", Answer = "You can borrow up to 10 items at a time." }
+        };
+
+        return View(faqList);
+    }
+
     }
 }
